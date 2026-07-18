e95001dd2ed93ee2cbedaf3d128c76eda9b519de
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-17 22:23:15 -0500

    remove dir changes

--- src/install_software.sh
+++ src/install_software.sh
@@ -35,7 +35,6 @@ success_web_packages=()
 install_web_packages() {
     [ "${#urls[@]}" -eq 0 ] && return
 
-    cd ~/Downloads || exit 1
     echo "Downloading extra applications from the web..."
 
     for i in "${#urls[@]}"; do
@@ -51,8 +50,6 @@ install_web_packages() {
     [ "${#success_web_packages[@]}" -gt 0 ] && sudo apt install "${success_web_packages[@]}" -y
 
     rm -f "${success_web_packages[@]}"
-
-    cd "${BASH_SOURCE%/*}" || exit 1
 
     if [ "${#failed_web_packages[@]}" -gt 0 ]; then
         echo -e "\033[0;31mERROR: The following web packages could not be downloaded, and thus were not installed:\033[0m"
