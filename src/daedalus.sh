20838ddf6773cc69acee332a3301309ae86d62c0
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-17 21:18:31 -0500

    fix typo

--- src/daedalus.sh
+++ src/daedalus.sh
@@ -5,7 +5,7 @@ clear
 while true; do
     echo -n "INPUT THE IP TO DAEDALUS: "
     read -r ip_address
-    if ! scp "daedalus@$ip_address:/mnt/storage/data/config/ssh_config ~/.ssh/config"; then
+    if ! scp "daedalus@$ip_address:/mnt/storage/data/config/ssh_config" ~/.ssh/config; then
         echo "ERROR: Unable to connect!"
         continue
     fi
