197474eea5528b4144c77377e7206e70ac3b2d59
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-18 16:16:08 -0500

    Add default vscode settings

--- src/mod_vscode.sh
+++ src/mod_vscode.sh
@@ -3,8 +3,32 @@ cd "${BASH_SOURCE%/*}" || exit 1
 
 ! which code &>/dev/null && echo -e "\033[0;31mERROR: VSCode is not installed, so extensions were not installed for it.\033[0m" && exit 1
 
+# Install all the extensions I want.
 extensions=()
 while read -r i; do
     extensions+=("--install-extension=$i")
 done < ../config/vscode.txt
 code "${extensions[@]}"
+
+# Default user preferences
+echo '{
+    "terminal.integrated.stickyScroll.enabled": false,
+    "workbench.startupEditor": "none",
+    "github.copilot.enable": {
+        "*": false,
+        "plaintext": false,
+        "markdown": false,
+        "scminput": false
+    },
+    "[python]": {
+        "editor.defaultFormatter": "ms-python.autopep8"
+    },
+    "git.addAICoAuthor": "off",
+    "chat.viewSessions.orientation": "stacked",
+    "editor.stickyScroll.enabled": false,
+    "terminal.integrated.allowChords": false,
+    "makefile.configureOnOpen": true,
+    "explorer.confirmDragAndDrop": false,
+    "explorer.confirmDelete": false,
+    "workbench.secondarySideBar.defaultVisibility": "hidden"
+}' > ~/.config/Code/User/settings.json
