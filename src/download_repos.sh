a78f371c28f57657933d55f10e7e22cc303c4761
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-18 13:55:42 -0500

    fix

--- src/download_repos.sh
+++ src/download_repos.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+dir="$(pwd)"
+
 cd ~ || exit 1
 
 ! dpkg -s git &>/dev/null && echo -e "\033[0;31mERROR: Git is not installed, so repositories cannot be downloaded.\033[0m" && exit 1
@@ -11,4 +14,4 @@ git config --global user.email "westerman.zachary@gmail.com"
 
 while read -r i; do
     [ ! -e "$i" ] && git clone "git@github.com:ZacharyWesterman/$i.git" --recurse-submodules
-done < ../config/repos.txt
+done < "$dir"/config/repos.txt
