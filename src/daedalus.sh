8d6b6faba5d54eae1606dfd49861fe0904be499f
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-17 21:22:01 -0500

    perms update

--- src/daedalus.sh
+++ src/daedalus.sh
@@ -2,10 +2,12 @@
 
 clear
 
+sudo echo
+
 while true; do
     echo -n "INPUT THE IP TO DAEDALUS: "
     read -r ip_address
-    if ! scp "daedalus@$ip_address:/mnt/storage/data/config/ssh_config" ~/.ssh/config; then
+    if ! sudo scp "daedalus@$ip_address:/mnt/storage/data/config/ssh_config" ~/.ssh/config; then
         echo "ERROR: Unable to connect!"
         continue
     fi
