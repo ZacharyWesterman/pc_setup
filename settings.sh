6e53498f843a782178a0a9bf75ba833387950d4c
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-18 16:00:46 -0500

    Add more settings

--- settings.sh
+++ settings.sh
@@ -12,5 +12,14 @@ gsettings set org.gnome.desktop.interface enable-hot-corners true
 # 24hr format for clock
 gsettings set org.gnome.desktop.interface clock-format '24h'
 
+# Don't show home folder on desktop
+gsettings set org.gnome.shell.extensions.ding show-home false
+
+# Show workspaces on all displays
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+# Include apps from only current workspace
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+
 # Set default list of applications to show in side bar
 gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Ptyxis.desktop', 'code_code.desktop', 'obsidian_obsidian.desktop', 'noisetorch.desktop']"
