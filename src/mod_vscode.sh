3d5e6ab649d1bd146a0d0a746d17a95027a2b633
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-18 13:34:15 -0500

    update vscode install to be from snap

--- src/mod_vscode.sh
+++ src/mod_vscode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "${BASH_SOURCE%/*}" || exit 1
 
-! dpkg -s code &>/dev/null && echo -e "\033[0;31mERROR: VSCode is not installed, so extensions were not installed for it.\033[0m" && exit 1
+! which code &>/dev/null && echo -e "\033[0;31mERROR: VSCode is not installed, so extensions were not installed for it.\033[0m" && exit 1
 
 extensions=()
 while read -r i; do
