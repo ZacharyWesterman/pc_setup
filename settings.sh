0e5f5b7ac0c4e9ae2cac1227427676ee324097e4
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-18 13:14:49 -0500

    Add basic script for updating gnome settings to my preferences

---
+++ settings.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Set dark mode
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
+# Enable hot corners
+gsettings set org.gnome.desktop.interface enable-hot-corners true
+
+# 24hr format for clock
+gsettings set org.gnome.desktop.interface clock-format '24h'
+
+# Set default list of applications to show in side bar
+gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Ptyxis.desktop']"
